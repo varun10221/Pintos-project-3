a2b41ee4cf98ee1efca8dde187496d4061954cea
Jamie Davis <davisjam@vt.edu>
2015-10-10 18:01:03 -0400

    frame: Declare private functions as static, comments, renaming, etc.
    I changed the order and names of some functions, and added some
    of my own.
    
    I also implemented (?) some of the higher-level functions by
    calling the not-yet-implemented lower-level ones.
    
    Here are the declarations so far:
    
    /* Private function declarations. */
    
    /* Nice way to get a frame. */
    static struct frame * frame_table_obtain_frame (void);
    
    static struct frame * frame_table_find_free_frame (void);
    
    /* Henchmen for the 'nice way'. */
    static struct frame * frame_table_make_free_frame (void);
    static struct frame * frame_table_get_eviction_victim (void);
    static void frame_table_evict_page_from_frame (struct frame *);
    
    static void frame_table_init_frame (struct frame *, id_t);
    
    ---------
    
    I agree that helper functions for evicting and getting back a page
    would be nice. One pair for mmap'd files and one pair for swap-able
    pages. Can you add these?
    
    ---------
    
    All of these functions should be ready for implementation.
    
    ---------
    
    One point to mention: as noted in 'struct page' (see c9e5f22c3),
    the page directory (aka HW page table) should be updated by
    the functions in frame.c, NOT by the supplemental page table.
    
    This is because the pagedir updates must be done while the page
    is guaranteed to be in the frame -- and this means they must
    be done while the frame is still locked. I don't want to give the
    SPT the ability to lock and unlock frames; this should be done
    by the frame table itself.
    
    The race condition I'm trying to avoid is as follows:
    
    Two processes, A and B.
    
    A: Store page P in the frame table in frame F
    B: Evict P from frame F, and replace with page Q
    A: Update pagedir to indicate that page P can be be found in F
    A: Access P: now we are accessing page Q
      -> SECURITY BREACH: A CAN NOW READ B'S INFORMATION!!
    
    Instead, we want behavior like this:
    
    A: Store page P in the frame table in frame F.
      -> This updates A's pagedir
    B: Evict P from frame F, and replace with page Q.
      -> This invalidate's A's pagedir entry for P.
    A: Access P: now we page fault and use frame_table_store_page() again
      to get it back into the table.

--- src/vm/frame.c
+++ src/vm/frame.c
@@ -1,5 +1,4 @@
-#include "vm/frame.h"
-
+#include "vm/frame.h" 
 #include <stdint.h>
 #include <debug.h>
 #include <list.h>
@@ -13,27 +12,19 @@
    Processes use the functions defined in frame.h to interact with this table. */
 frame_table_t system_frame_table;
 
-/* Convert KADDR to frame id. TODO */
-static id_t 
-frame_table_addr_to_ix (void *kaddr)
-{
-  ASSERT (kaddr != NULL);
-  ASSERT ( (uint32_t) kaddr % PGSIZE == 0);
+/* Private function declarations. */
 
-  return 0;
-}
+/* Nice way to get a frame. */
+static struct frame * frame_table_obtain_frame (void);
 
-/* Initialize this frame. */
-static void 
-frame_table_init_frame (struct frame *fr, id_t id)
-{
-  ASSERT (fr != NULL);
+static struct frame * frame_table_find_free_frame (void);
 
-  fr->id = id;
-  lock_init (&fr->lock);
-  fr->status = FRAME_EMPTY;
-  fr->pg = NULL;
-}
+/* Henchmen for the 'nice way'. */
+static struct frame * frame_table_make_free_frame (void);
+static struct frame * frame_table_get_eviction_victim (void);
+static void frame_table_evict_page_from_frame (struct frame *);
+
+static void frame_table_init_frame (struct frame *, id_t);
 
 /* Basic life cycle. */
 
@@ -43,6 +34,7 @@ frame_table_init (void)
 {
   size_t i;
 
+  system_frame_table.n_free_entries = FRAME_TABLE_N_FRAMES;
   system_frame_table.usage = bitmap_create (FRAME_TABLE_N_FRAMES);
   ASSERT (system_frame_table.usage != NULL);
     
@@ -71,67 +63,54 @@ frame_table_destroy (void)
 
 /* Storing and releasing pages. */
 
-/* Put this page into a frame.
-   The frame's information is stored in PG for the caller
-   to deal with.
-   TODO */
+/* Put locked page PG into a frame.
+   Update the page directory for each of its owners. */
 void 
 frame_table_store_page (struct page *pg)
 {
   ASSERT (pg != NULL);
-    /*acquire global lock*/
-   /*do a bitscan of frame_bit map,by calling free_frame _find*/
-   /*acquire lock on entry */ /*check frame status*/
-   /* release global lock */
-  /*in free frame, do a struct frame update */
-   /*update the details in corresponding page struct*/
-  /*flip the bitmap*/
-   /*update in spt */
 
+  /* Page must not be in a page already. */
+  ASSERT (pg->status != PAGE_RESIDENT);
 
+  struct frame *fr = frame_table_obtain_frame ();
+  if (fr == NULL)
+    PANIC ("frame_table_store_page: Could not allocate a frame."); 
+
+  /* Tell each about the other. */
+  fr->status = FRAME_OCCUPIED;
+  fr->pg = pg;
+  pg->location = fr;
+  fr->popularity = POPULARITY_START;
+
+  /* TODO Update the page directory of each owner. */
+  ASSERT (0 == 1);
+
+  /* Page safely in frame. */
+  lock_release (&fr->lock);
 }
 
-/* Release resources associated with this page; process is done with it. 
+/* Release resources associated with locked page PG; process is done with it. 
    If an mmap'd file, flush changes to backing file. 
    TODO */
 void 
 frame_table_release_page (struct page *pg)
-{ ASSERT (pg != NULL);
+{ 
+  ASSERT (pg != NULL);
   /* Get the frame from  pg->location */
   /*revert  the frame parameters, probably a  frame_init */
   /*acquire lock and do a bitflip */
   /*make the other parameters to match init in case of release due to process exit */
- /*TODO:Can we have a separate function for writing mmap file? */
- /* Im assuming writing back mmap file involves block_write and some other support 
-   infra needed */
- 
+ /*TODO:Can we have a separate function for writing mmap file? 
+   Im assuming writing back mmap file involves block_write and some other support 
+   infra needed 
+   
+   JD: Yes, I agree. I think separate functions for evicting mmap and non-mmap pages are needed.
+   Please add declarations for these.
+   */
 }
 
-/*searches the global frame table and retieves a free frame.Not thread safe */
-struct frame *
-free_frame_find (void)
-{
-  /*do a bit map scan and retrieve the first free frame after locking the table */
-  /*release the lock*
- /*then call swap out to put the frame in swap and retuen the free index */
-
-   return frame;
-}
-
-
-/*searches the global table and evicts the first non-pinned frame*/
-static id_t
-frame_evict (void)
-{
-  /*get a global lock on table*/
-  /*search for first non-pinned frame  and  call swap_table_store_page*/
- /*for mmap file range, check if the page is dirty ,if so write to file,
-   else set the frame free and return frame */
-  /*may need to panic in an unlikely scenario of all frames pinned */
- return id; //variable of type id_t  
-}
-
-/* (Put page PG into a frame and) pin  it there. 
+/* (Put locked page PG into a frame and) pin it there. 
    It will not be evicted until a call to 
     - frame_table_release_page(), or
     - frame_table_unpin_page()
@@ -143,45 +122,137 @@ frame_table_pin_page (struct page *pg)
  /*change the frame_status to pinned */
 }
 
-/* Allow page PG to be evicted from its frame,
-   if it is in a frame. 
+/* Allow locked resident page PG to be evicted from its frame.
    TODO. */
 void 
 frame_table_unpin_page (struct page *pg)
 {
   ASSERT (pg != NULL);
+
+  ASSERT (pg->status == PAGE_RESIDENT)
+  struct frame *fr = (struct frame *) pg->location;
+
+  ASSERT (fr != NULL);
+  ASSERT (fr->status == FRAME_PINNED);
+
+  fr->status = FRAME_OCCUPIED;
 }
 
+/* Private helper functions. */
 
-#if 0
+/* This function will evict the page from a frame
+   and return the frame to you, locked.
+
+   Returns NULL if all frames have their page pinned. */
+static struct frame * frame_table_make_free_frame (void)
+{
+  struct frame *victim = frame_table_get_eviction_victim ();
+  if (victim != NULL)
+    frame_table_evict_page_from_frame (victim);
+  return victim;
+}
+
+/* Identify a victim frame whose page can be evicted.
+   Return the locked frame.
+
+   If no candidate is identified, returns NULL. */
+static struct frame * frame_table_get_eviction_victim (void)
+{
+  /* Get a global lock on table. */ 
+  /* Search for first frame whose page is not pinned (for now!) and: */
+  /* May need to panic in an unlikely scenario of all frames pinned. */
+  return NULL;
+}
+
+/* Evict the page in locked frame FR. */
+static void frame_table_evict_page_from_frame (struct frame *fr)
+{
+  ASSERT (fr != NULL);
+  /* - For mmap'd file page, check if the page is dirty. 
+       If so write to file, else set the frame free and return frame.
+     - For other types of pages, call swap_table_store_page. */
+}
+
+/* Initialize frame FR. */
+static void 
+frame_table_init_frame (struct frame *fr, id_t id)
+{
+  ASSERT (fr != NULL);
+
+  fr->id = id;
+  lock_init (&fr->lock);
+  fr->status = FRAME_EMPTY;
+  fr->pg = NULL;
+}
+
+/* Allocate a frame for a page. 
+ 
+   Locate a free frame, mark it as used, lock it, and return it.
+   May have to evict a page to obtain a free frame.
+   If no free or evict-able frames are available, returns null. */
+static struct frame * frame_table_obtain_frame (void)
+{
+  struct frame *fr = frame_table_find_free_frame ();
+  if (fr == NULL)
+    fr = frame_table_make_free_frame ();
+
+  return fr;
+}
+
+/* Searches the system frame table and retieves a free locked frame. 
+   Returns NULL if no free frames are available (necessitates eviction). */
+static struct frame *
+frame_table_find_free_frame (void)
+{
+  /*do a bit map scan and retrieve the first free frame after locking the table */
+  /*release the lock, 
+    then call swap out to put the frame in swap and retuen the free index */
+  struct frame *fr = NULL;
+  return fr;
+}
+
+/*
 Just an overview of functions so far, 
 (to make sure if we have API's to handle all scenario)
 i)frame table init and frame init.
 ii)functions to store and evict a frame
 iii)function to discard frames
-iv)functions to pin , unpin.
+iv)functions to pin, unpin.
 v)bit map_updation as a part of store and evict.
 vi)connecting swap and frame, for reading from swap, we can define the logic from spt, for storing , we can use the functions in frame.c
+*/
 
+#if 0
+/* Verify that id ID is valid. */
+static inline void frame_table_validate_id (id_t id)
+{
+  ASSERT (0 <= id && id < FRAME_TABLE_N_FRAMES);
+}
+
+static struct frame * frame_table_id_to_frame (id_t);
+static id_t frame_table_frame_to_id (struct frame *);
+
+/* Look up the frame associated with id ID. */
+static struct frame * 
+frame_table_id_to_frame (id_t id)
+{
+  frame_table_validate_id (id);
+  struct frame *frames = (struct frame *) system_frame_table.entries;
+  return &frames[id];
+}
+
+/* Look up the id associated with frame FR. */
+static id_t 
+frame_table_frame_to_id (struct frame *fr)
+{
+  ASSERT (fr != NULL); 
+  return fr->id;
+}
 #endif
 
 #if 0
 
 /* Allocating and freeing entries. */
-
-/* Identify a frame table entry whose page can be evicted.
-   Return the locked FTE.
-
-   If no candidate is identified, returns NULL.
-   to get eviction candidate
-
- */
-struct frame * frame_table_get_eviction_candidate (frame_table_t *ft)
-{
-  ASSERT (ft != NULL);
-
-  return NULL;
-}
 
 /* Swap this frame out. */ 
 swap_id_t frame_table_swap_out (struct frame *fte)
@@ -273,4 +344,16 @@ void frame_table_release_frame (struct page *pg)
 
   lock_release (&ft->usage_lock);
 }
+
+/* Convert KADDR to frame id. TODO */
+static id_t 
+frame_table_addr_to_ix (void *kaddr)
+{
+  ASSERT (kaddr != NULL);
+  ASSERT ( (uint32_t) kaddr % PGSIZE == 0);
+
+  return 0;
+}
+
+
 #endif
