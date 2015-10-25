9b6b5334ae8fa1099b18e0cc0466c68a0a06a788
varuns92 <varuns92@vt.edu>
2015-10-25 13:43:43 -0400

    Added basic struct declaration and function names.     yet to write a API's description.

--- src/filesys/cache.c
+++ src/filesys/cache.c
@@ -1,1 +1,30 @@
 #include "cache.h"
+
+
+
+#define CACHE_FLUSH_ INTERVAL 1  /*keeping it as 1s for now */
+
+/*The buffer_cache_table will be global
+ and will be used to store file blocks */
+struct buffer_cache_table
+{
+  uint32_t n_free_cache_blocks; /*No. of free blocks ,may use int8_t */
+  uint32_t n_total_cache_blocks; /*Total no. of cache blocks in table */
+  struct cache_block  *cache_block; /*Array for cache_table,table
+       utmost will hold 64 sectors wortth of data */
+  struct lock buffer_cache_lock; /*Incase , if any atomic operations needs to be performed */
+
+}
+
+/*Entry in Buffer_acche_table */ /*can be named as block maybe ?*/
+struct cache_block
+{
+  block_sector_t block_sector_id; /*sector id */
+  bool is_dirty;                  /*to check if the block has been written to */
+  bool is_valid;                  /*If the data is still valid or has been invalidated */
+  uint32_t readers_count;        /*No. of readers , reading from the block */
+  uint32_t writers_count;        /*No. of writers writing to the block */
+  uint32_t pending_requests;     /*Pending requests waiting due to read/write regulation */
+  struct lock cache_block_lock; /*lock on this particular block alone */
+}
+
--- src/filesys/cache.h
+++ src/filesys/cache.h
@@ -1,4 +1,39 @@
 #ifndef FILESYS_CACHE_H
 #define FILESYS_CACHE_H
 
+#include <stdint.h>
+#include <inttypes.h>
+#include <stddef.h>
+
+
+
+
+
+struct  buffer_cache_table;
+struct  cache_block;
+
+/*Basic life cycle */
+void buffer_cache_table init (size_t);
+void buffer_cache_table_destroy (void);
+
+/* Getting and releasing access to cache blocks */
+struct cache_block * cache_get_block (disk_sector_t , bool );
+void cache_put_block (struct cache_block *);
+
+/*Storing and releasing blocks in cache block table */
+void * cache_read_block (struct cache_block *b);
+void * cache_zero_block (struct cache_block *b);
+
+/*Marking the state of cache block */
+void cache_mark_block_dirty (struct cache_block *b);
+void cache_mark_block_invalid (struct cache_block *b);
+
+/*Periodic flushing of cache */
+void buffer_cache_table_flush_data (void);
+  
+
+
+
+
+
 #endif /* filesys/cache.h */
