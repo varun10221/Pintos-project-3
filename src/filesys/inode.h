1b27d989a0ad6308827a7373ebf4b981c2b9b041
Jamie Davis <davisjam@vt.edu>
2015-11-03 16:21:58 -0500

    inode: new API for inode_create
    Add a TYPE so we know if it's a file or a directory.
    
    Needed because we have inodes of two types: FILE and DIRECTORY
    (which means we have two different blocksizes: DATA_BLOCKSIZE
      and METADATA_BLOCKSIZE)

--- src/filesys/inode.h
+++ src/filesys/inode.h
@@ -9,8 +9,15 @@ struct bitmap;
 struct inode;
 #define INODE_SIZE BLOCK_SECTOR_SIZE
 
+/* Inodes can be one of these types. */
+enum inode_type
+{
+  INODE_FILE,
+  INODE_DIRECTORY
+};
+
 void inode_init (void);
-bool inode_create (block_sector_t, off_t);
+bool inode_create (block_sector_t, enum inode_type, off_t);
 struct inode *inode_open (block_sector_t);
 struct inode *inode_reopen (struct inode *);
 block_sector_t inode_get_inumber (const struct inode *);
