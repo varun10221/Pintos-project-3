varun10221/Pintos-project-3

commit e16d7eef27ec69a1cf1692d0eb351977a4f482c9

free-map: cache changes without flushing every time
Problem: Circular relationship as follows:

free_map_allocate calls file_write
file_write calls inode_write, which eventually realizes that
it needs to allocate disk space and calls free_map_allocate.

Solution: This feels a bit hack-y, but:
  free_map_allocate and free_map_release now do NOT sync to disk.
  This is only done on free_map_close and new function free_map_commit.

src/filesys/free-map.c
@@ -8,10 +8,16 @@
 static struct file *free_map_file;   /* Free map file. */
 static struct bitmap *free_map;      /* Free map, one bit per sector. */
 
+/* free_map changes from free_map_allocate and free_map_release are cached.
+   They are written out on free_map_close and free_map_commit. */
+
 /* Initializes the free map. */
 void
 free_map_init (void) 
 {
+  free_map = NULL;
+  free_map_file = NULL;
+
   free_map = bitmap_create (block_size (fs_device));
   if (free_map == NULL)
     PANIC ("bitmap creation failed--file system device is too large");
@@ -22,20 +28,17 @@ free_map_init (void)
 /* Allocates CNT consecutive sectors from the free map and stores
    the first into *SECTORP.
    Returns true if successful, false if not enough consecutive
-   sectors were available or if the free_map file could not be
-   written. */
+   sectors were available. */ 
 bool
 free_map_allocate (size_t cnt, block_sector_t *sectorp)
 {
   block_sector_t sector = bitmap_scan_and_flip (free_map, 0, cnt, false);
-  if (sector != BITMAP_ERROR
-      && free_map_file != NULL
-      && !bitmap_write (free_map, free_map_file))
-    {
-      bitmap_set_multiple (free_map, sector, cnt, false); 
-      sector = BITMAP_ERROR;
-    }
-  if (sector != BITMAP_ERROR)
+  if (sector == BITMAP_ERROR)
+  {
+    bitmap_set_multiple (free_map, sector, cnt, false); 
+    sector = BITMAP_ERROR;
+  }
+  else
     *sectorp = sector;
   return sector != BITMAP_ERROR;
 }
@@ -46,7 +49,14 @@ free_map_release (block_sector_t sector, size_t cnt)
 {
   ASSERT (bitmap_all (free_map, sector, cnt));
   bitmap_set_multiple (free_map, sector, cnt, false);
-  bitmap_write (free_map, free_map_file);
+}
+
+/* If free map has been initialized, commits it to disk. */
+void free_map_commit (void)
+{
+  /* Could be NULL during initialization. */
+  if (free_map_file != NULL)
+    bitmap_write (free_map, free_map_file);
 }
 
 /* Opens the free map file and reads it from disk. */
@@ -73,7 +83,7 @@ void
 free_map_create (void) 
 {
   /* Create inode. */
-  if (!inode_create (FREE_MAP_SECTOR, bitmap_file_size (free_map)))
+  if (!inode_create (FREE_MAP_SECTOR, INODE_FILE, bitmap_file_size (free_map)))
     PANIC ("free map creation failed");
 
   /* Write bitmap to file. */

src/filesys/free-map.h
@@ -13,5 +13,6 @@ void free_map_close (void);
 
 bool free_map_allocate (size_t, block_sector_t *);
 void free_map_release (block_sector_t, size_t);
+void free_map_commit (void);
 
 #endif /* filesys/free-map.h */

commit 511005620e20952630c27c4ab15332c595d938b8

free_map_allocate: Bug in handling error
Problem was introduced in e16d7eef27e.
Mis-interpreted the initial condition.

src/filesys/free-map.c
@@ -33,14 +33,12 @@ bool
 free_map_allocate (size_t cnt, block_sector_t *sectorp)
 {
   block_sector_t sector = bitmap_scan_and_flip (free_map, 0, cnt, false);
-  if (sector == BITMAP_ERROR)
+  if (sector != BITMAP_ERROR)
   {
-    bitmap_set_multiple (free_map, sector, cnt, false); 
-    sector = BITMAP_ERROR;
-  }
-  else
     *sectorp = sector;
-  return sector != BITMAP_ERROR;
+    return true;
+  }
+  return false;
 }
 
 /* Makes CNT sectors starting at SECTOR available for use. */